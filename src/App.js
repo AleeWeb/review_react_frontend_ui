5aa3ae8683c191360575748122f5645223a6bbc3
AleeWeb <aalexanderpr@gmail.com>
2018-10-09 15:28:36 -0400

    Set up for running on heroku

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from "react";
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import {
+  Row, Col, Card, CardBody, CardTitle, CardText, CardImg
+} from "reactstrap";
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      'reviews': []
+    }
+  }
+  componentDidMount() {
+    this.getReviews();
+  }
+
+  getReviews() {
+    fetch('http://127.0.0.1:8000/reviews/')
+    .then(results => results.json())
+    .then(results => this.setState({'reviews': results}));
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ul>
+      {this.state.reviews.map(function(review, index) {
+        return <ReviewItem review={review} key={index} />
+      })}
+    </ul>
     );
   }
 }
+
+const ReviewItem = ({ review }) => (
+  <Row className="ReviewItem">
+    <Col xs="3" />
+    <Col xs="12" sm="6">
+      <Card>
+        <CardImg top width="250" src={review.image}></CardImg>
+        <CardBody>
+          <CardTitle>
+            {review.review_title}
+          </CardTitle>
+          <CardText>
+            {review.user_review}
+          </CardText>
+        </CardBody>
+      </Card>
+    </Col>
+  </Row>
+)
 
 export default App;
